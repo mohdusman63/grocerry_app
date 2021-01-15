75a79cf1601249d7d01494c465bbaa44ea7fad30
chandraprakashrajpoot <mohdusmanali63@gmail.com>
2021-01-15 17:14:58 +0530

    player added commit

---
+++ hackerrank/string.js
@@ -0,0 +1,10 @@
+console.log('=============>')
+let s="usmanazmi"
+let r=s.slice(0,3)
+s=r
+console.log(r)
+// for(let i=0;i<s.length;i++){
+//     str.substr(0, startIndex);
+
+// }
+console.log(s)
